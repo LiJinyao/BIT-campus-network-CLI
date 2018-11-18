37871809e8399b75129b4d289b2d457e80c8a96f
LiJinyao <lijinyao@outlook.com>
2018-11-18 22:09:02 +0800

    Hide password in command line

--- index.js
+++ index.js
@@ -4,21 +4,58 @@ function printHelp() {
     console.log(`
     用法
     登陆：
-    node index.js login your-account your-password
+    node index.js login your-account
     注销：
     node index.js logout your-account
     `)
 }
+
+async function getPassword(prompt) {
+    process.stdout.write(prompt)
+    return new Promise((resolve, reject) => {
+        const stdin = process.stdin
+        stdin.setRawMode(true)
+        stdin.setEncoding('utf8')
+    
+        var password = ''
+        stdin.on('data', function (ch) {
+            ch = ch.toString('utf8')
+            switch (ch) {
+            case "\n":
+            case "\r":
+            case "\u0004":
+                // They've finished typing their password
+                process.stdout.write('\n')
+                stdin.setRawMode(false)
+                stdin.pause()
+                resolve(password)
+                break
+            case "\u0003":
+                // Ctrl-C
+                process.stdout.write('\n')
+                stdin.setRawMode(false)
+                stdin.pause()
+                process.exit(0)
+                break
+            default:
+                // More passsword characters
+                process.stdout.write('*')
+                password += ch
+                break
+            }
+        })
+    })
+    
+}
+
 async function exec(args) {
-    if(args.length < 4) {
+    if(args.length !== 4) {
         printHelp()
     }
     switch (args[2]) {
         case 'login':
-            if(args.length != 5) {
-                printHelp()
-            }
-            const loginResponse = await login(args[3], args[4])
+            const password = await getPassword('Password: ')
+            const loginResponse = await login(args[3], password)
             if(loginResponse.suc_msg === 'login_ok') {
                 console.log(`login successfully`)
                 let usage = await usageStatus(loginResponse)
